bf9b8a41746cb2da8cf6ded58988cd2bbba5e416
Krizam Byanju <krizambyanju@gmail.com>
2024-10-02 22:16:31 +0545

    Image Gallery

---
+++ script.js
@@ -0,0 +1,49 @@
+let photo = document.querySelectorAll("img");
+photo.style.width = "200px";
+
+photo.addEventListener('click',()=>{
+    imgs.style.width ="800px";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let Body=document.body;
+// let Images=document.querySelector(".Images");
+// let changeSize =1;
+// let Largephoto = document.querySelector(".Largephoto");
+// Body.append(Largephoto);
+
+// let changeImage =()=>{
+//     if(changeSize == 1){
+//         Body.classList.add("Large");
+        
+
+
+//         changeSize = 0;
+//     }
+//     else if(changeSize == 0){
+//         Body.classList.add("body1");
+    
+
+//     }
+ 
+
+// }
+// Images.addEventListener("click",changeImage);
